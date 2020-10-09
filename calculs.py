a5747965f24644afa84423b95c9c276cb2d1a46e
Ewan GRIGNOUX LEVERT <ewangl@kalitey>
2020-10-09 16:24:18 +0200

    Virus worsened

--- calculs.py
+++ calculs.py
@@ -5,7 +5,7 @@
 import doctest
 import math
 
-def inhabitants(month, contamination=0.05):
+def inhabitants(month, contamination=1):
     '''
     A village has 25 inhabitants, 
     after each month the number of inhabitants increases by 20% (is multiplied by 1.2)
@@ -13,18 +13,19 @@ def inhabitants(month, contamination=0.05):
     This function returns the number of inhabitants based on the number of months elapsed.
     PRECONDITIONS: month is a positive integer
     POST CONDITIONS: Inhabitants must be a positive integer
-    >>> inhabitants(0, 0.05)
-    (25, 20)
-    >>> inhabitants(1, 0.05)
-    (28, 22)
-    >>> inhabitants(5, 0.05)
-    (46, 30)
-    >>> inhabitants(20, 0.05)
-    (533, 113)
+    >>> inhabitants(0)
+    (25, 1, 0)
+    >>> inhabitants(1)
+    (28, 2, 0)
+    >>> inhabitants(5)
+    (46, 32, 1)
+    >>> inhabitants(20)
+    (-48790, 720631, 36980)
     '''
     if month == 0:
-        return (25,20)
-    
-    return (math.floor(inhabitants(month-1)[0]*1.2-2), math.floor(inhabitants(month-1)[1]*(1+contamination)))
+        return (25,1,0,0)
 
+    h, i, d = inhabitants(month-1, contamination)
+
+    return (math.floor(h * 1.2 - 2 - d), math.floor(i*(1+contamination)-d), math.floor(i*0.1))
 doctest.testmod()
--- main.py
+++ main.py
@@ -10,10 +10,12 @@ class MyInterface(FloatLayout):
 
     def cliquer(self):
         self.month = self.ids.month_cursor.value
-        self.contamination = self.ids.contamination_cursor.value
-        self.people, self.infected = calculs.inhabitants(self.month, self.contamination)
+        self.people, self.infected, useless = calculs.inhabitants(self.month)
+        if self.people <0:
+            self.people =0
         if self.infected > self.people:
             self.infected = self.people
+        
         self.sick = int(0.75*self.infected)
         self.healthy_carrier = int(0.25*self.infected)
         
@@ -22,7 +24,7 @@ class MyInterface(FloatLayout):
             Rectangle(pos = self.pos, size=(800,500))
 
         for _ in range(self.people):
-            Person(self.canvas)
+            Person(self.canvas, self)
         
         self.ids.reponse.text = str(self.people) + " inhabitants"
         self.ids.sick.text = str(self.sick) + " sick"
@@ -30,9 +32,9 @@ class MyInterface(FloatLayout):
         self.ids.infected.text = str(self.infected) + " infected"
 
 class Person(Widget):
-    def __init__(self, canvas):
+    def __init__(self, canvas, interface):
         self.canvas = canvas
-        self.pos = [random.randint(1,750), random.randint(1,425)]
+        self.pos = [random.randint(1,interface.size[0]-30), random.randint(1,interface.size[1]*0.8-55)]
 
         with self.canvas:
             Rectangle(source='perso.png', pos = self.pos, size=(30,55))
